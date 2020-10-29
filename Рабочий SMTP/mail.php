69822a11f3e803fe55a45a9ed6025bc6e160b603
Gleb <gleb.kosheev@yandex.ru>
2020-10-30 02:27:16 +0300

    whatahelldoyouboysthinkthatthisis?fuckingfairyland?pullingonyourweeweeslikeapackoffuckingfruitys

--- Рабочий SMTP/mail.php
+++ Рабочий SMTP/mail.php
@@ -1,57 +1,34 @@
-<?php 
+<?php
 
 require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
-
-$name = $_POST['user_name'];
+$nickname = $_POST['user_name'];
 $phone = $_POST['user_phone'];
 $email = $_POST['user_email'];
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-if(strlen($name)<3 )
-{
- echo "Правильно оформите обязательные поля";
-}
-/*
-elseif(!filter_var($email,FILTER_VALIDATE_EMAIL)) {
- 	echo "Правильно оформите обязательные поля";
- }
- */
-elseif (filter_var($phone,FILTER_VALIDATE_INT)!=true) {
- 	echo "Правильно оформите обязательные поля";
- } 
- 
- else{
 
+if (strlen($nickname) < 3 || !filter_var($email, FILTER_VALIDATE_EMAIL))
+    echo "Правильно оформите обязательные поля";
+else {
+    $mail->isSMTP(); // Set mailer to use SMTP
+    $mail->Host = 'smtp.mail.ru'; // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true; // Enable SMTP authentication
+    $mail->Username = 'toxa699@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+    $mail->Password = '55561999275556y'; // Ваш пароль от почты с которой будут отправляться письма
+    $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'toxa699@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '55561999275556y'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
-
-$mail->setFrom('toxa699@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('toxa699@mail.ru');     // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
-$mail->AltBody = '';
-
-if(!$mail->send()) {
-    echo 'Error';
-} else {
-    header('location: thank-you.html');
-}
+    $mail->setFrom('toxa699@mail.ru'); // от кого будет уходить письмо?
+    $mail->addAddress('toxa699@mail.ru'); // Кому будет уходить письмо
+    $mail->isHTML(true); // Set email format to HTML
+    $mail->Subject = 'Заявка с тестового сайта';
+    $mail->Body = '' . 'user nickname: ' . $nickname . '<br>User email: ' . $email;
+    $mail->AltBody = '';
+    try {
+        $mail->send();
+        header('location: thank-you.html');
+    } catch (phpmailerException $e) {
+    }
 }
 
 ?>
