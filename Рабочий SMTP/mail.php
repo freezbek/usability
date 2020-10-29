de2276cba4fb4fa515c30f13fd93f86a1de6f795
freezbek <toxa-n99@mail.ru>
2020-10-30 00:58:21 +0300

    Изменение папки "рабочий SMTP"

--- Рабочий SMTP/mail.php
+++ Рабочий SMTP/mail.php
@@ -7,19 +7,26 @@ $mail->CharSet = 'utf-8';
 $name = $_POST['user_name'];
 $phone = $_POST['user_phone'];
 $email = $_POST['user_email'];
-
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
+if(filter_var($name,FILTER_SANITIZE_STRING)!="" ||
+	filter_var($phone,FILTER_VALIDATE_INT)!=false || filter_var($email,FILTER_VALIDATE_EMAIL)!="")
+{
+ echo "Правильно оформите обязательные поля";
+}
+else {
+
+
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'dzharuzov@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '$dk820&123'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'toxa699@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '55561999275556y'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('dzharuzov@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('gomudusu@p33.org');     // Кому будет уходить письмо 
+$mail->setFrom('toxa699@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('toxa699@mail.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -37,4 +44,6 @@ if(!$mail->send()) {
 } else {
     header('location: thank-you.html');
 }
+}
+
 ?>
