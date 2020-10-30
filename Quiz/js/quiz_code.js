f545d7057dedd18be5c6418be9d7d5753393117a
Gleb <gleb.kosheev@yandex.ru>
2020-10-30 23:14:20 +0300

    changes in quiz_code

--- Quiz/js/quiz_code.js
+++ Quiz/js/quiz_code.js
@@ -1,18 +1,24 @@
 window.onload = function () {
-    var h_2 = document.getElementsByTagName('h2');
-    var h_3 = document.getElementsByTagName('h3');
 
-    var second_metodic_a=0;var second_metodic_b=0; var second_metodic_c=0;var second_metodic_d=0;
-    var third_metodic_a=0;var third_metodic_b=0; var third_metodic_c=0;
-    var fourth_metodic_a=0;var fourth_metodic_b=0; var fourth_metodic_c=0;
 
-    h_2 = h_2[0];
-    h_2.innerText = 'Методика 1. «Матрица выбора профессии» ';
-    h_3 = h_3[0];
-    h_3.innerText = '1. С кем или с чем Вы бы хотели работать? Какой объект деятельности Вас привлекает?';
+    var testName = document.getElementsByTagName('h1');
+    var testChapter = document.getElementsByTagName('h2');
+    var question = document.getElementsByTagName('h3');
+
+    let second_a = 0, second_b = 0, second_c = 0, second_d = 0;
+    let third_a = 0, third_b = 0, third_c = 0;
+    let fourth_a = 0, fourth_b = 0, fourth_c = 0;
+
+    testChapter = testChapter[0];
+    testChapter.innerText = test_chapter_name[1];
+    question = question[0];
+    question.innerText = '1. С кем или с чем Вы бы хотели работать? Какой объект деятельности Вас привлекает?';
+    testName = testName[0];
+    testName.innerText = 'Дистанционное тестирование по теме «Карьерный потенциал»';
 
     let result = {};
-    let step = 20;
+    let step = 0;
+
     function showQuestion(questionNumber) {
         document.querySelector('.question').innerHTML = quiz[step]['q'];
         let answer = '';
@@ -20,67 +26,87 @@ window.onload = function () {
             answer += `<li data-v='${key}' class="answer-variant">${quiz[step]['a'][key]}</li>`;
         }
         document.querySelector('.answer').innerHTML = answer;
-
     }
 
     document.onclick = function (event) {
         event.stopPropagation();
         if (event.target.classList.contains('answer-variant') && step < quiz.length) {
-            
+
             if (result[event.target.dataset.v] != undefined) {
                 result[event.target.dataset.v]++;
-            }
-            else {
+            } else {
                 result[event.target.dataset.v] = 0;
             }
             step++;
-            if (step == quiz.length) {
+            if (step === quiz.length) {
                 document.querySelector('.question').remove();
                 document.querySelector('.answer').remove();
+                testChapter.remove();
+                testName.remove();
                 showResult();
-            }
-            else {
+            } else {
                 showQuestion();
             }
-           // console.log( result[event.target]);
         }
         if (event.target.classList.contains('reload-button')) {
             location.reload(result[event.target.dataset.v]);
         }
-        //if (step <10) {}
         if (step >= 10 && step < 20) {
-            h_3.textContent = '2. Чем бы Вы хотели заниматься? Какой вид деятельности Вас привлекает?';
+            question.textContent = '2. Чем бы Вы хотели заниматься? Какой вид деятельности Вас привлекает?';
         }
         if (step >= 20 && step < 40) {
-            h_2.textContent = 'Методика 2. «Изучение статусов профессиональной идентичности»';
-            h_3.textContent = '';
-             if (event.target.dataset.v == "_a") {second_metodic_a++;}
-            if (event.target.dataset.v == '_b') {second_metodic_b++;}
-             if (event.target.dataset.v == '_c') {second_metodic_c++;}
-              if (event.target.dataset.v == '_d') {second_metodic_d++;}
+            testChapter.textContent = test_chapter_name[2];
+            question.textContent = '';
+            if (event.target.dataset.v == "_a") {
+                second_a++;
+            }
+            if (event.target.dataset.v == '_b') {
+                second_b++;
+            }
+            if (event.target.dataset.v == '_c') {
+                second_c++;
+            }
+            if (event.target.dataset.v == '_d') {
+                second_d++;
+            }
         }
         if (step >= 40 && step < 80) {
-            h_2.textContent = 'Методика 3.  «Большая пятерка личностных качеств»';
-            h_3.textContent = '';
-            if (event.target.dataset.v == '_a') {third_metodic_a++;}
-            if (event.target.dataset.v == '_b') {third_metodic_b++;}
-             if (event.target.dataset.v == '_c') {third_metodic_c++;}
+            testChapter.textContent = test_chapter_name[3];
+            //question.textContent = '';
+            if (event.target.dataset.v == '_a') {
+                third_a++;
+            }
+            if (event.target.dataset.v == '_b') {
+                third_b++;
+            }
+            if (event.target.dataset.v == '_c') {
+                third_c++;
+            }
         }
-        if (step >= 80 && step <121) {
-            h_2.textContent = 'Методика 4. «Мотивация выбора профессии» ';
-            h_3.textContent = '';
-            if (event.target.dataset.v == '_a') {fourth_metodic_a++;}
-            if (event.target.dataset.v == '_b') {fourth_metodic_b++;}
-             if (event.target.dataset.v == '_c') {fourth_metodic_c++;}
+        if (step >= 80 && step <= 121) {
+            testChapter.textContent = test_chapter_name[4];
+            //question.textContent = '';
+            if (event.target.dataset.v == '_a') {
+                fourth_a++;
+            }
+            if (event.target.dataset.v == '_b') {
+                fourth_b++;
+            }
+            if (event.target.dataset.v == '_c') {
+                fourth_c++;
+            }
         }
-        //console.log(result);
-        console.log(second_metodic_a,second_metodic_b,second_metodic_c,second_metodic_d,"второй метод");
-        //console.log(third_metodic_a,third_metodic_c,third_metodic_b,third_metodic_c,"третий метод");
-        //console.log(fourth_metodic_a,fourth_metodic_b,fourth_metodic_c,"четвёртый метод");
+        console.log(second_a, second_b, second_c, second_d, "второй метод");
+        console.log(third_a, third_c, third_b, third_c, "третий метод");
+        console.log(fourth_a, fourth_b, fourth_c, "четвёртый метод");
     }
 
     function showResult() {
-        let key = Object.keys(result).reduce(function (a, b) { return result[a] > result[b] ? a : b });
+        let key = Object.keys(result).reduce(function (a, b) {
+            return result[a] > result[b] ? a : b
+        });
+
+
         let div = document.createElement('div');
         div.classList.add('result');
         div.innerHTML = answers[key]['description'];
@@ -96,58 +122,63 @@ window.onload = function () {
         reloadButton.classList.add('reload-button');
         document.querySelector('main').appendChild(reloadButton);
 
-        var ctx = document.getElementById('myChart').getContext('2d');
+
+        //график
+
         var chartOptions = {
-          scales: {
-            yAxes: [{
-              barPercentage: 0.5,
-              gridLines: {
-                display: false
-              }
-            }],
-            xAxes: [{
-              gridLines: {
-                zeroLineColor: "black",
-                zeroLineWidth: 2
-              },
-              ticks: {
-                min: 0,
-                max: 6500,
-                stepSize: 1
-              },
-              scaleLabel: {
-                display: true,
-                labelString: "Density in kg/m3"
-              }
-            }]
-          },
-          elements: {
-            rectangle: {
-              borderSkipped: 'left',
+            scales: {
+                yAxes: [{
+                    barPercentage: 0.5,
+                    gridLines: {
+                        display: false
+                    }
+                }],
+                xAxes: [{
+                    gridLines: {
+                        zeroLineColor: "black",
+                        zeroLineWidth: 2
+                    },
+                    ticks: {
+                        min: 0,
+                        max: 6500,
+                        stepSize: 1
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Density in kg/m3"
+                    }
+                }]
+            },
+            elements: {
+                rectangle: {
+                    borderSkipped: 'left',
+                }
             }
-          }
         };
+
+
+        var ctx = document.getElementById('myChart').getContext('2d');
         var chart = new Chart(ctx, {
             // The type of chart we want to create
             type: 'bar',
 
             // The data for our dataset
             data: {
-                labels: ['a', 'b','c','d',],
+                labels: ['a', 'b', 'c', 'd',],
                 datasets: [{
                     label: 'Вторая методика',
-                    backgroundColor: 'rgb(255,99,132)',
+                    backgroundColor: 'rgb(255, 99, 132)',
                     borderColor: 'rgb(255, 99, 132)',
-                    data: [second_metodic_a,second_metodic_b,second_metodic_c,second_metodic_d]
-       }]
+                    data: [second_a, second_b, second_c, second_d]
+                }]
             },
 
             // Configuration options go here
             options: {}
-         });
+        });
         console.log(key);
     }
+
     showQuestion();
 }
-
 
