cda7454ad0dc34d50b86872c40f4c890fe9f1754
Gleb Kosheev <gleb.kosheev@yandex.ru>
2020-11-02 19:40:46 +0300

    Boss of this Gym
    Hey buddy, I think you've got the wrong door, the leather club's two blocks down.
    
    Fuck↗You↘
    
    Oh, Fuck♂You leather man. Maybe you and I should settle it right here on the ring if you think your so tough.
    
    Oh yea? I'll kick your ass!
    
    Ha! Yeah right man. Let's go! Why don't you get out of that leather stuff? I'll strip down out of this and we'll settle it right here in the ring. What do you say?
    
    Yeah, no problem buddy!
    
    You got it. Get out of that uh, jabroni outfit.
    
    Yeah, smart ass.
    
    I'll show you who's the boss of this gym.

---
+++ test_login_PHP/index.php
@@ -0,0 +1,51 @@
+<?php
+include_once 'dbh.inc.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="login">
+    <h1>Login</h1>
+    <form action="authenticate.php" method="post">
+        <label for="username">
+            <i class="fas fa-user"></i>
+        </label>
+        <input type="text" name="username" placeholder="Username" id="username" required>
+        <label for="password">
+            <i class="fas fa-lock"></i>
+        </label>
+        <input type="password" name="password" placeholder="Password" id="password" required>
+        <input type="submit" value="Login">
+    </form>
+</div>
+
+
+<?php
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "root";
+$dbName = "logpass";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+
+$sql = "SELECT * FROM logpass;";
+$result = mysqli_query($conn, $sql);
+$resultCheck = mysqli_num_rows($result);
+
+if ($resultCheck > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo $row['1'];
+    }
+}
+?>
+
+</body>
+</html>
